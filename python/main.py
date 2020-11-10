34c4fd56d3022385d35de9fa97f5073b4905ab27
Elwood He <elwoodhe@ElwooddeMacBook-Pro.local>
2020-11-10 04:58:37 -0500

    Add dataset, ver 0.3.2

---
+++ python/main.py
@@ -0,0 +1,10 @@
+# ------------------------------------
+# Stats 506 midterm project
+# ARIMA model for time series data
+# Author: Zhilin he, Group 3
+# Updated: November 9, 2020
+# ------------------------------------
+
+# Import data
+import pandas as pd
+df = pd.read_csv()
